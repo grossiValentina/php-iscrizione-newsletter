36999dbfbfa2573c347ea9b91eb85b26704ac966
grossiValentina <grossivalentina435@gmail.com>
2023-12-13 18:08:37 +0100

    add: commenti

--- index.php
+++ index.php
@@ -17,18 +17,20 @@
     <div class="container mt-4 pt-4 border border-3 border-info">
         <h1 class="text-center pb-3">Ciao utente!</h1>
 
+        <!-- condizione per emeil sbagliata e alert-danger  -->
         <?php if (isset($_GET["email"]) && !filter_email($email)) { ?>
             <div class='alert alert-danger'>La mail non è corretta</div>
         <?php } ?>
 
+        <!-- condizione per emeil giusta e alert-success  -->
         <?php if (isset($_GET["email"]) && filter_email($email)) { ?>
             <div class='alert alert-success'>La mail è corretta</div>
         <?php } ?>
 
+        <!-- form email -->
         <form class="text-center pb-4" action="index.php" method="GET">
-            <!-- input email -->
+            
             <label class="me-3" for="email">inserisci la tua e-mail</label>
-
             <input class="w-50" type="text" id="email" name="email">
 
             <button type="submit" class="btn btn-info ms-3">conferma</button>
@@ -37,4 +39,4 @@
     </div>
 </body>
 
-</html>
+</html >
